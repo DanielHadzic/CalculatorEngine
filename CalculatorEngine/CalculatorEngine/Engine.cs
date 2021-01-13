c550997f905dd97890944c46c2613055752e336c
Daniel Hadzic <dhadzic@ambrac.nl>
2021-01-13 15:58:51 +0100

    add solution and project

---
+++ CalculatorEngine/CalculatorEngine/Engine.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculatorEngine
+{
+    public class Engine
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        } 
+    }
+}
